ff63f91e7eca2282b419a3f2d1c4456efbb1f73d
Paul Fries <pfries@Pauls-iMac-Pro.local>
2020-07-24 11:43:42 -0700

    Setting up the react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "Ynotes-app-bucket-pjf"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://txm0vkfftg.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_cd5K1tE14",
+      APP_CLIENT_ID: "48b9pe3h8mm3nfmqg4g2v2a01f",
+      IDENTITY_POOL_ID: "us-east-2:1f8df148-09d5-49af-aac7-363453109220"
+    }
+  };
