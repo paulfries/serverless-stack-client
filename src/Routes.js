1ad884a7829b35736d4d4b456ae669d4247b266f
System Administrator <root@Pauls-iMac-Pro.local>
2020-07-24 14:49:52 -0700

    Deploy Chem101 Dev UI

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Notes from "./containers/Notes";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
@@ -9,10 +17,22 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
       {/* Finally, catch all unmatched routes */}
-<Route>
-  <NotFound />
-</Route>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-2",
-      BUCKET: "Ynotes-app-bucket-pjf"
+      BUCKET: "notes-app-2-api-dev-attachmentsbucket-gdpv52kum0gl"
     },
     apiGateway: {
       REGION: "us-east-2",
-      URL: "https://txm0vkfftg.execute-api.us-east-2.amazonaws.com/prod"
+      URL: "https://azh2lsaj1l.execute-api.us-east-2.amazonaws.com/dev"
     },
     cognito: {
       REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_cd5K1tE14",
-      APP_CLIENT_ID: "48b9pe3h8mm3nfmqg4g2v2a01f",
-      IDENTITY_POOL_ID: "us-east-2:1f8df148-09d5-49af-aac7-363453109220"
+      USER_POOL_ID: "us-east-2_O690CocoB",
+      APP_CLIENT_ID: "2knbsi2tq68jusvbs8r5aq0jdo",
+      IDENTITY_POOL_ID: "us-east-2:c1f9cd76-fae4-4e97-a738-e6399dcf4591"
     }
   };
